dc42c4c6c4c72943c5b99c9520dec8fb84926f9f
LiamMcCausland <106557278+LiamMcCausland@users.noreply.github.com>
2022-06-08 15:30:06 -0500

    Add files via upload

--- Pokemon/src/finalPokemon/GameScreen.java
+++ Pokemon/src/finalPokemon/GameScreen.java
@@ -10,9 +10,14 @@ import static finalPokemon.StartMenu.squirtleIsActive;
 import finalPokemon.pokemon.Bulbasaur;
 import finalPokemon.pokemon.Charmander;
 import finalPokemon.pokemon.Cyndaquil;
+import finalPokemon.pokemon.Pokemon;
 import javax.swing.JFrame;
 import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
 import finalPokemon.pokemon.Squirtle;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -29,8 +34,11 @@ public class GameScreen extends JFrame {
     public boolean bu;
     public boolean sq;
 
-    public  String currentEnemy;
-     public  int enemyHp;
+    public Pokemon currentEnemy;
+    public Pokemon friendly;
+    public int enemyHp;
+
+    public double friendlyHp;
 
     /**
      * Creates new form GameScreen
@@ -64,8 +72,8 @@ public class GameScreen extends JFrame {
         button3 = new java.awt.Button();
         button4 = new java.awt.Button();
         output = new javax.swing.JLabel();
-        pokemonHpText = new javax.swing.JLabel();
-        enemyHpText = new javax.swing.JLabel();
+        enemyHealth = new javax.swing.JLabel();
+        friendlyHealth = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(null);
@@ -103,30 +111,19 @@ public class GameScreen extends JFrame {
         getContentPane().add(output);
         output.setBounds(400, 220, 570, 100);
 
-        pokemonHpText.setText("jLabel2");
-        getContentPane().add(pokemonHpText);
-        pokemonHpText.setBounds(190, 390, 50, 20);
+        enemyHealth.setText("health");
+        getContentPane().add(enemyHealth);
+        enemyHealth.setBounds(800, 370, 33, 16);
 
-        enemyHpText.setText("jLabel2");
-        getContentPane().add(enemyHpText);
-        enemyHpText.setBounds(640, 400, 50, 16);
+        friendlyHealth.setText("heatlh");
+        getContentPane().add(friendlyHealth);
+        friendlyHealth.setBounds(170, 370, 50, 20);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void button1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_button1ActionPerformed
-        if (bu) {
-            double dmg = seedBomb();
-            enemyHp -= dmg;
-            output.setText("Bulbasaur used Seed Bomb and did " + dmg + " damage!");
-            if (enemyHp < 0) output.setText(currentEnemy + " has fainted");
-   
-        } else if (ch) {
-            scratch();
-
-        } else if (sq) {
-            tackle();
-        }
+        chooseMove();
     }//GEN-LAST:event_button1ActionPerformed
 
     private void setFrame() {
@@ -138,25 +135,16 @@ public class GameScreen extends JFrame {
     }
 
     private void initialize(boolean ch, boolean bu, boolean sq) {
-        if (bu) {
-            bulbasaur = new Bulbasaur(jLabel1);
-            button1.setLabel(bulbasaur.attack0);
-            button2.setLabel(bulbasaur.attack1);
-            button3.setLabel(bulbasaur.attack2);
-            button4.setLabel(bulbasaur.attack3);
-        } else if (ch) {
-            charmander = new Charmander(jLabel1);
-            button1.setLabel(charmander.attack0);
-            button2.setLabel(charmander.attack1);
-            button3.setLabel(charmander.attack2);
-            button4.setLabel(charmander.attack3);
-        } else if (sq) {
-            squirtle = new Squirtle(jLabel1);
-            button1.setLabel(squirtle.attack0);
-            button2.setLabel(squirtle.attack1);
-            button3.setLabel(squirtle.attack2);
-            button4.setLabel(squirtle.attack3);
-        }
+        if (bu)      bulbasaur = new Bulbasaur(jLabel1);   
+        else if (ch) charmander = new Charmander(jLabel1);
+        else if (sq) squirtle = new Squirtle(jLabel1);
+        
+        friendlyHp = getFriendly().hp;
+        friendlyHealth.setText(getFriendly().hp + "/" + getFriendly().hp);
+        button1.setLabel(getFriendly().attack0);
+        button2.setLabel(getFriendly().attack1);
+        button3.setLabel(getFriendly().attack2);
+        button4.setLabel(getFriendly().attack3);
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -165,10 +153,10 @@ public class GameScreen extends JFrame {
     private java.awt.Button button3;
     private java.awt.Button button4;
     private javax.swing.JLabel enemy;
-    private javax.swing.JLabel enemyHpText;
+    private javax.swing.JLabel enemyHealth;
+    private javax.swing.JLabel friendlyHealth;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel output;
-    private javax.swing.JLabel pokemonHpText;
     // End of variables declaration//GEN-END:variables
 
     private void level(int lvlNum) {
@@ -194,17 +182,8 @@ public class GameScreen extends JFrame {
         enemy.setOpaque(true);
     }
 
-    private double seedBomb() {
-        double dmg = bulbasaur.atk * 0.15;
-        return dmg;
-    }
-
-    private void scratch() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    private double tackle() {
-        double dmg = bulbasaur.atk * 0.05;
+    private double attack(Pokemon attack, double multiplier) {
+        double dmg = attack.atk * multiplier;
         return dmg;
     }
 
@@ -228,8 +207,77 @@ public class GameScreen extends JFrame {
     private void cyndaquil() {
         output.setText("A wild cyndaquil has appeared!");
         cyndaquil = new Cyndaquil(enemy);
-        currentEnemy =  cyndaquil.name;
+        currentEnemy = cyndaquil;
         enemyHp = cyndaquil.hp;
 
+        //Enemy health text
+        enemyHealth.setOpaque(true);
+        enemyHealth.setVisible(true);
+        enemyHealth.setText(enemyHp + "/" + cyndaquil.hp);
+        add(enemyHealth);
+
     }
+
+    protected void enemyTurn() {
+        String attackName = "";
+        double dmg = 0;
+        switch (random(1, 1)) {
+            case 1:
+                dmg = attack(getEnemy(), 0.2);
+                friendlyHp -= dmg;
+                friendlyHealth.setText(friendlyHp + "/" + getFriendly().hp);
+                attackName = getEnemy().attack0;
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            default:
+                break;
+        }
+        output.setText(getEnemy().name + " used " + attackName + " and did " + dmg + " damage!");
+    }
+
+    private void chooseMove() {
+        if (bu) {
+            double dmg = attack(bulbasaur, 0.12);
+            enemyHp -= dmg;
+            enemyHealth.setText(enemyHp + "/" + cyndaquil.hp);
+            output.setText("Bulbasaur used Seed Bomb and did " + dmg + " damage!");
+            button1.setEnabled(false);
+            button2.setEnabled(false);
+            button3.setEnabled(false);
+            button4.setEnabled(false);
+            if (enemyHp <= 0) {
+                output.setText(currentEnemy + " has fainted!");
+                System.exit(0);
+            }
+        } else if (ch) {
+            //scratch();
+
+        } else if (sq) {
+            //tackle();
+        }
+        enemyTurn();
+    }
+
+    private Pokemon getEnemy() {
+        return currentEnemy;
+    }
+
+    private Pokemon getFriendly() {
+        if (bu) {
+            friendly = bulbasaur;
+        } else if (ch) {
+            friendly = charmander;
+
+        } else if (sq) {
+            friendly = squirtle;
+        }
+
+        return friendly;
+    }
+
 }
